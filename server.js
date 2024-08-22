50b0dd32d6d853556070ebc5297ed61b5359f5a4
Chand-007 <130752042+Chand-007@users.noreply.github.com>
2024-08-22 21:13:37 +0530

    Added POST Endpoint

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const app = express()
 
-
-
-
+app.post('/identify',(req,res)=>{
+    
+   
+})
 
 app.listen(5000,()=>{
     console.log("Server Listening on port 5000")
